6711b75ff2f726f07bfd76cbae42c7917568e7fe
AnnoyingDog <jan.vlasman99@gmail.com>
2020-11-07 18:00:01 +0100

    Reeeefactoring part 1

--- src/main/resources/static/floormat.js
+++ src/main/resources/static/floormat.js
@@ -2,39 +2,40 @@
  * adds texture to the floor
  * color, displacement and roughness for the floor
  */
-function floormat(){
-    var floorMat;
-floorMat = new THREE.MeshStandardMaterial({
-  roughness: 0.6,
-  color: 0xffffff,
-  metalness: 0.1,
-  bumpScale: 0.001,
-  side: THREE.DoubleSide
-});
-var textureLoader = new THREE.TextureLoader();
-textureLoader.load("textures/Concrete_009_COLOR.jpg", function(map) {
-  map.wrapS = THREE.RepeatWrapping;
-  map.wrapT = THREE.RepeatWrapping;
-  map.anisotropy = 4;
-  map.repeat.set(30, 20);
-  floorMat.map = map;
-  floorMat.needsUpdate = true;
-});
-textureLoader.load("textures/Concrete_009_DISP.jpg", function(map) {
-  map.wrapS = THREE.RepeatWrapping;
-  map.wrapT = THREE.RepeatWrapping;
-  map.anisotropy = 4;
-  map.repeat.set(30, 20);
-  floorMat.bumpMap = map;
-  floorMat.needsUpdate = true;
-});
-textureLoader.load("textures/Concrete_009_ROUGH.jpg", function(map) {
-  map.wrapS = THREE.RepeatWrapping;
-  map.wrapT = THREE.RepeatWrapping;
-  map.anisotropy = 4;
-  map.repeat.set(30, 20);
-  floorMat.roughnessMap = map;
-  floorMat.needsUpdate = true;
-});
-return floorMat;
+function floormat() {
+  const textureLoader = new THREE.TextureLoader();
+
+  const floorMat = new THREE.MeshStandardMaterial({
+    roughness: 0.6,
+    color: 0xffffff,
+    metalness: 0.1,
+    bumpScale: 0.001,
+    side: THREE.DoubleSide
+  });
+
+  textureLoader.load("textures/Concrete_009_COLOR.jpg", function (map) {
+    map.wrapS = THREE.RepeatWrapping;
+    map.wrapT = THREE.RepeatWrapping;
+    map.anisotropy = 4;
+    map.repeat.set(30, 20);
+    floorMat.map = map;
+    floorMat.needsUpdate = true;
+  });
+  textureLoader.load("textures/Concrete_009_DISP.jpg", function (map) {
+    map.wrapS = THREE.RepeatWrapping;
+    map.wrapT = THREE.RepeatWrapping;
+    map.anisotropy = 4;
+    map.repeat.set(30, 20);
+    floorMat.bumpMap = map;
+    floorMat.needsUpdate = true;
+  });
+  textureLoader.load("textures/Concrete_009_ROUGH.jpg", function (map) {
+    map.wrapS = THREE.RepeatWrapping;
+    map.wrapT = THREE.RepeatWrapping;
+    map.anisotropy = 4;
+    map.repeat.set(30, 20);
+    floorMat.roughnessMap = map;
+    floorMat.needsUpdate = true;
+  });
+  return floorMat;
 }
--- src/main/resources/static/robotSound.js
+++ src/main/resources/static/robotSound.js
@@ -1,26 +1,20 @@
 /**
- * adds sound to the robot
- * @param {camera thats in the scene} camera
- * count is there so there is just one instance of the sound
- * Refdistance for volume increase/decrease over distance
- * returns sound to the robot
- * sound used is made and edited by us
+ * Handels audio of robot
  */
-
+let firstTime = true;
 function robotSound(camera) {
-  var listener = new THREE.AudioListener();
+  const listener = new THREE.AudioListener();
+  const sound = new THREE.PositionalAudio(listener);
+  const audioLoader = new THREE.AudioLoader();
   camera.add(listener);
-  var sound = new THREE.PositionalAudio(listener);
-  var audioLoader = new THREE.AudioLoader();
-  var count = 0;
-  audioLoader.load("sounds/untitled.ogg", function(buffer) {
+  audioLoader.load("sounds/untitled.ogg", function (buffer) {
     sound.setBuffer(buffer);
     sound.setRefDistance(0.1);
     sound.setVolume(0.3);
     sound.setLoop(true);
-    if (count == 0) {
+    if (firstTime) {
       sound.play();
-      count++;
+      firstTime = false;
     }
   });
   return sound;
